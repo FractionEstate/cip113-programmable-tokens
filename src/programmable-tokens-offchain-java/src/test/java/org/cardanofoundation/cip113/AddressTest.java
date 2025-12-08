a8ed511a442078701f7f7ef31f7f6630df5d070e
Giovanni Gargiulo <giovanni@Giovannis-MacBook-Pro.local>
2025-12-08 10:24:52 +0000

    chore: imports and tests

--- src/programmable-tokens-offchain-java/src/test/java/org/cardanofoundation/cip113/AddressTest.java
+++ src/programmable-tokens-offchain-java/src/test/java/org/cardanofoundation/cip113/AddressTest.java
@@ -1,6 +1,10 @@
 package org.cardanofoundation.cip113;
 
 import com.bloxbean.cardano.client.account.Account;
+import com.bloxbean.cardano.client.exception.CborDeserializationException;
+import com.bloxbean.cardano.client.plutus.spec.BytesPlutusData;
+import com.bloxbean.cardano.client.plutus.spec.PlutusData;
+import com.bloxbean.cardano.client.util.HexUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +20,41 @@ public class AddressTest {
 
 
     }
+
+    @Test
+    public void serde() throws CborDeserializationException {
+
+        var version = "1.2.0";
+
+        var plutusBytes = BytesPlutusData.of(HexUtil.encodeHexString(version.getBytes()));
+
+        var bytesPlutusData = (BytesPlutusData)PlutusData.deserialize(plutusBytes.serializeToBytes());
+
+        var actualVersion = bytesPlutusData.getValue();
+
+        log.info(new String(HexUtil.decodeHexString(new String(actualVersion))));
+
+
+    }
+
+    @Test
+    public void serde2() throws CborDeserializationException {
+
+        var version = "1.2.0";
+
+        var plutusBytes = BytesPlutusData.of(HexUtil.encodeHexString(version.getBytes()));
+
+        var inlineDatum = plutusBytes.serializeToBytes();
+
+        var bytesPlutusData = (BytesPlutusData)PlutusData.deserialize(inlineDatum);
+
+        var actualVersion = bytesPlutusData.getValue();
+
+        log.info(new String(HexUtil.decodeHexString(new String(actualVersion))));
+
+
+    }
+
+
 
 }
